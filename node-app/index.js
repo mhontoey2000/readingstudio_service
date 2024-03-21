52e81a3854870d25e8834cc991bb6f15f7ebc3df
mhontoey2000 <teimiku@gmail.com>
2024-03-22 01:23:14 +0700

    อันแรก

---
+++ node-app/index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+app.get('/',(req,res)=> res.json({message:'hello world'}))
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
