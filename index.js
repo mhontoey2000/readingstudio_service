862e222887df9e3ee9ec4a025e658a9fd3d9cd65
mhontoey2000 <teimiku@gmail.com>
2024-03-24 01:54:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8080
-const cors = require('cors')
+// const cors = require('cors')
 
 
 app.get('/', (req, res) => res.send('Hello World'))
 app.listen(PORT, () => {
     console.log(`Server is running on port : ${PORT}`)
 })
-const helper = require('./upload');
-const sendMail = require('./sendmail');
+// const helper = require('./upload');
+// const sendMail = require('./sendmail');
