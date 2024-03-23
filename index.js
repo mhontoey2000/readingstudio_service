53a0699b9d31e8d6f6990733cfcaccfba5f6a333
mhontoey2000 <teimiku@gmail.com>
2024-03-24 01:51:21 +0700

    new2

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8080
+const cors = require('cors')
+
 
 app.get('/', (req, res) => res.send('Hello World'))
 app.listen(PORT, () => {
     console.log(`Server is running on port : ${PORT}`)
 })
+const helper = require('./upload');
+const sendMail = require('./sendmail');
