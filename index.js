ab95ece2e0056d20c95b3cec92cc4b6b6b794d9a
mhontoey2000 <teimiku@gmail.com>
2024-03-24 02:08:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = process.env.PORT || 8080
 const cors = require('cors')
 const helper = require('./upload');
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 // const sendMail = require('./sendmail');
 
 app.get('/', (req, res) => res.send('Hello World'))
