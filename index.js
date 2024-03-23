61f87baa4fd9f3f3506d14c40092e81d2033c521
mhontoey2000 <teimiku@gmail.com>
2024-03-24 00:22:58 +0700

    อัพเดท3

--- index.js
+++ index.js
@@ -2,6 +2,59 @@ const express = require('express')
 const app = express()
 const posts = require('./posts')
 
+const cors = require('cors')
+const multer = require('multer');
+const bodyParser = require('body-parser')
+// const mysql = require('mysql2');
+// const dotenv = require("dotenv")
+const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
+// const { json } = require('react-router-dom');
+// process.env.ACCESS_TOKEN_SECRET = 'doraemon';
+// dotenv.config()
+const helper = require('./upload');
+const sendMail = require('./sendmail');
+
+// function generateAccessToken(user) {
+//   return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 60 * 60 * 24 * 30 });
+// }
+// function decodedToken(token) {
+//   return new Promise((resolve, reject) => {
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//       if (err) {
+//         // Token is invalid or has expired
+//         resolve(false);
+//       } else {
+//         // Token is valid;
+//         resolve(true);
+//       }
+//     });
+//   });
+// }
+
+app.use(cors());
+app.use(express.json());
+
+// app.use(bodyParser.json({ limit: '500mb' }));
+// app.use(bodyParser.urlencoded({ limit: '500mb', extended: true }));
+// app.use(express.json({limit: '1000mb'}));
+// app.use(express.urlencoded({extended: true ,limit: '1000mb'}));
+// app.use(bodyParser.raw({ type: 'image/*', limit: '160MB' }));
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'datareadingstudio',
+//     password: ''
+// });
+// connection.connect((err) => {
+//     if (!!err) {
+//         console.log(err);
+//     } else {
+//         console.log('Connected...');
+//     }
+//   });
+
+const router = require('express-promise-router')()
 app.get('/posts', (req, res) => {
     res.json(posts)
 })
@@ -11,75 +64,6 @@ app.get('/', (req, res) => {
 })
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running."))
-
-const cors = require('cors')
-const multer = require('multer');
-const bodyParser = require('body-parser')
-// const mysql = require('mysql2');
-const dotenv = require("dotenv")
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { json } = require('react-router-dom');
-// const connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     database: 'datareadingstudio',
-//     password: ''
-// });
-process.env.ACCESS_TOKEN_SECRET = 'doraemon';
-dotenv.config()
-const helper = require('./upload');
-const sendMail = require('./sendmail');
-
-function generateAccessToken(user) {
-  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 60 * 60 * 24 * 30 });
-}
-function decodedToken(token) {
-  return new Promise((resolve, reject) => {
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-      if (err) {
-        // Token is invalid or has expired
-        resolve(false);
-      } else {
-        // Token is valid;
-        resolve(true);
-      }
-    });
-  });
-}
-
-
-
-app.use(cors());
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Ahoy!' });
-});
-
-app.get('/api/key', function (req, res) {
-  res.json({ key: process.env.PASS_KEY });
-});
-app.use(bodyParser.json({ limit: '500mb' }));
-app.use(bodyParser.urlencoded({ limit: '500mb', extended: true }));
-app.use(express.json({limit: '1000mb'}));
-app.use(express.urlencoded({extended: true ,limit: '1000mb'}));
-app.use(bodyParser.raw({ type: 'image/*', limit: '160MB' }));
-
-
-// connection.connect((err) => {
-//     if (!!err) {
-//         console.log(err);
-//     } else {
-//         console.log('Connected...');
-//     }
-  
-//   });
-
-
-//const { request } = require('express')
-const router = require('express-promise-router')()
-
 
 router.get('/tbl',async (req,res,next) => {
     try {
