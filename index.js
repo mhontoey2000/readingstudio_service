4ebd52e4d5b327d58b03b41c9acec2cbfc4faf6f
mhontoey2000 <teimiku@gmail.com>
2024-03-24 02:46:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,30 @@ const { json } = require('react-router-dom');
 process.env.ACCESS_TOKEN_SECRET = 'doraemon';
 dotenv.config()
 
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 60 * 60 * 24 * 30 });
+  }
+  function decodedToken(token) {
+    return new Promise((resolve, reject) => {
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          // Token is invalid or has expired
+          resolve(false);
+        } else {
+          // Token is valid;
+          resolve(true);
+        }
+      });
+    });
+  }
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json({ limit: '500mb' }));
+app.use(bodyParser.urlencoded({ limit: '500mb', extended: true }));
+app.use(express.json({limit: '1000mb'}));
+app.use(express.urlencoded({extended: true ,limit: '1000mb'}));
+app.use(bodyParser.raw({ type: 'image/*', limit: '160MB' }));
+
 app.get('/', (req, res) => res.send('Hello World'))
 app.listen(PORT, () => {
     console.log(`Server is running on port : ${PORT}`)
