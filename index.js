617a3c4299c9794fa89008529feb6a1a84748ec4
mhontoey2000 <teimiku@gmail.com>
2024-03-24 02:12:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 8080
 const cors = require('cors')
 const helper = require('./upload');
 const sendMail = require('./sendmail');
-const multer = require('multer');
+// const multer = require('multer');
 
 app.get('/', (req, res) => res.send('Hello World'))
 app.listen(PORT, () => {
