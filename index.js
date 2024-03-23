aea4d7bba59ad46df58c1fe479814b14d222cb57
mhontoey2000 <teimiku@gmail.com>
2024-03-24 01:55:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8080
-// const cors = require('cors')
+const cors = require('cors')
 
 
 app.get('/', (req, res) => res.send('Hello World'))
